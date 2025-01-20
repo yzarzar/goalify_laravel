0593611d0e97b69f6924d864881dd12d428ccd62
yzarzar <zar31528@gmail.com>
2025-01-20 11:20:27 +0630

    🔧 Validate task due_date within goal and milestone range

--- app/Http/Controllers/MilestoneController.php
+++ app/Http/Controllers/MilestoneController.php
@@ -46,6 +46,16 @@ class MilestoneController extends BaseController
                 return $this->sendNotFound('Goal not found');
             }
 
+            // Validate due date is within goal's date range
+            $dueDate = $request->input('due_date');
+            if ($dueDate < $goal->start_date || $dueDate > $goal->end_date) {
+                return $this->sendError(
+                    'Invalid due date',
+                    ['due_date' => ['Due date must be between goal\'s start date and end date']],
+                    422
+                );
+            }
+
             // Merge the goal_id with the validated data
             $data = array_merge($request->validated(), ['goal_id' => $goal_id]);
 
@@ -111,6 +121,18 @@ class MilestoneController extends BaseController
                     ['status' => ['Status is automatically determined by task completion']],
                     422
                 );
+            }
+
+            // Validate due date is within goal's date range if it's being updated
+            if ($request->has('due_date')) {
+                $dueDate = $request->input('due_date');
+                if ($dueDate < $goal->start_date || $dueDate > $goal->end_date) {
+                    return $this->sendError(
+                        'Invalid due date',
+                        ['due_date' => ['Due date must be between goal\'s start date and end date']],
+                        422
+                    );
+                }
             }
 
             // Update the milestone
